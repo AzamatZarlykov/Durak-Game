69c7992a1771bc46ecd659f4409c7236309b9066
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-04-29 04:20:03 +0200

    adding the folder that handles all the middleware

---
+++ DurakGame.WebSite/DurakGame.Server/Middleware/DurakServerMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace DurakGame.Server.Middleware
+{
+    public class DurakServerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DurakServerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+         
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket websocket = await context.WebSockets.AcceptWebSocketAsync();
+                Console.WriteLine("WebSocket Connected");
+
+                await ReceiveMessage(websocket, async (result, buffer) =>
+                {
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        Console.WriteLine("Message Received");
+                        Console.WriteLine($"Message : {Encoding.UTF8.GetString(buffer, 0, result.Count)}");
+                        return;
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        Console.WriteLine("Received Close Message");
+                        return;
+                    }
+                });
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        private async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                    cancellationToken: CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
+        }
+    }
+}
---
+++ DurakGame.WebSite/DurakGame.Server/Middleware/DurakServerMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace DurakGame.Server.Middleware
+{
+    public static class DurakServerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseWebSocketServer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<DurakServerMiddleware>();
+        }
+    }
+}
