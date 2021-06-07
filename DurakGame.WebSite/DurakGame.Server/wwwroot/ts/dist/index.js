b1b1d13375987f4ebe9efbc70b1b77a341d9d9d8
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-06-07 04:44:53 +0200

    update the ts file

--- DurakGame.WebSite/DurakGame.Server/wwwroot/ts/dist/index.js
+++ DurakGame.WebSite/DurakGame.Server/wwwroot/ts/dist/index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+let ws;
+let serverurl = "ws://localhost:5000";
+// Initiate WebSocket connection
+function connect(url) {
+    ws = new WebSocket(url);
+    ws.onopen = onOpen;
+    ws.onmessage = onMessage;
+    ws.onerror = onError;
+    ws.onclose = onClose;
+}
+function onOpen(event) {
+    console.log("connected");
+}
+function onMessage(event) {
+}
+function onError(event) {
+}
+function onClose(event) {
+}
+connect(serverurl);
--- DurakGame.WebSite/DurakGame.Server/wwwroot/ts/src/index.ts
+++ DurakGame.WebSite/DurakGame.Server/wwwroot/ts/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello world");
+let ws : any 
+let serverurl : string = "ws://localhost:5000"
+
+// Initiate WebSocket connection
+function connect(url: string) : void { 
+    ws = new WebSocket(url);
+    ws.onopen = onOpen;
+    ws.onmessage = onMessage;
+    ws.onerror = onError;
+    ws.onclose = onClose;
+}
+
+function onOpen (event: any) : void {
+    console.log("connected");
+}
+
+function onMessage(event: any) : void {
+
+}
+
+function onError(event: any) : void {
+
+}
+
+function onClose(event: any) : void  {
+
+}
+
+connect(serverurl);
