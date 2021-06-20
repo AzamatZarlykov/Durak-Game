ed14de2a7514f792e6a7579015e6675ac927f2bf
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-06-20 23:17:30 +0200

    set up the server using the websockets with typescript

---
+++ DurakGame.WebSite/DurakGame.Server/wwwroot/src/socket.ts
@@ -0,0 +1,115 @@
+﻿let connectionUrl = document.getElementById("connectionUrl") as HTMLInputElement;
+let connectButton = document.getElementById("connectButton") as HTMLButtonElement;
+let stateLabel = document.getElementById("stateLabel");
+let sendMessage = document.getElementById("sendMessage") as HTMLInputElement;
+let sendButton = document.getElementById("sendButton") as HTMLButtonElement;
+let commsLog = document.getElementById("commsLog");
+let closeButton = document.getElementById("closeButton") as HTMLButtonElement;
+
+let socket: WebSocket;
+
+let scheme = document.location.protocol === "https:" ? "wss" : "ws";
+let port = document.location.port ? (":" + document.location.port) : "";
+
+console.log('hello');
+
+connectionUrl.value = scheme + "://" + document.location.hostname + port + "/ws";
+
+function updateState() {
+    function disable() {
+        sendMessage.disabled = true;
+        sendButton.disabled = true;
+        closeButton.disabled = true;
+    }
+    function enable() {
+        sendMessage.disabled = false;
+        sendButton.disabled = false;
+        closeButton.disabled = false;
+    }
+
+    connectionUrl.disabled = true;
+    connectButton.disabled = true;
+
+    if (!socket) {
+        disable();
+    } else {
+        switch (socket.readyState) {
+            case WebSocket.CLOSED:
+                stateLabel.innerHTML = "Closed";
+                disable();
+                connectionUrl.disabled = false;
+                connectButton.disabled = false;
+                break;
+            case WebSocket.CLOSING:
+                stateLabel.innerHTML = "Closing...";
+                disable();
+                break;
+            case WebSocket.CONNECTING:
+                stateLabel.innerHTML = "Connecting...";
+                disable();
+                break;
+            case WebSocket.OPEN:
+                stateLabel.innerHTML = "Open";
+                enable();
+                break;
+            default:
+                stateLabel.innerHTML = "Unknown WebSocket State: " +
+                    htmlEscape(socket.readyState.toString());
+                disable();
+                break;
+        }
+    }
+}
+
+closeButton.onclick = function () {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    socket.close(1000, "Closing from client");
+};
+
+sendButton.onclick = function () {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    let data = sendMessage.value;
+    socket.send(data);
+    commsLog.innerHTML += '<tr>' +
+        '<td class="commslog-client">Client</td>' +
+        '<td class="commslog-server">Server</td>' +
+        '<td class="commslog-data">' + htmlEscape(data) + '</td></tr>';
+};
+
+connectButton.onclick = function () {
+    stateLabel.innerHTML = "Connecting";
+    socket = new WebSocket(connectionUrl.value);
+    socket.onopen = function (event) {
+        updateState();
+        commsLog.innerHTML += '<tr>' +
+            '<td colspan="3" class="commslog-data">Connection opened</td>' +
+            '</tr>';
+    };
+    socket.onclose = function (event) {
+        updateState();
+        commsLog.innerHTML += '<tr>' +
+            '<td colspan="3" class="commslog-data">Connection closed. Code: ' +
+            htmlEscape(event.code.toString()) + '. Reason: ' + htmlEscape(event.reason) + '</td>' +
+            '</tr>';
+    };
+    socket.onerror = updateState;
+    socket.onmessage = function (event) {
+        commsLog.innerHTML += '<tr>' +
+            '<td class="commslog-server">Server</td>' +
+            '<td class="commslog-client">Client</td>' +
+            '<td class="commslog-data">' + htmlEscape(event.data) + '</td></tr>';
+    };
+};
+
+function htmlEscape(str: string) {
+    return str.toString()
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
--- DurakGame.WebSite/DurakGame.Server/wwwroot/ts/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare let ws: any;
-declare let serverurl: string;
-declare function connect(url: string): void;
-declare function onOpen(event: any): void;
-declare function onMessage(event: any): void;
-declare function onError(event: any): void;
-declare function onClose(event: any): void;
