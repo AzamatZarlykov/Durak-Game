973405c551f8104452c718ef2be7439936b2791c
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-07-17 01:25:57 +0200

    added the player hand class

--- DurakGame.WebSite/DurakGame.Server/Library/GamePlayer/Hand/PlayerHand.cs
+++ DurakGame.WebSite/DurakGame.Server/Library/GamePlayer/Hand/PlayerHand.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using DurakGame.Server.Library.GameCard;
+
 namespace DurakGame.Server.Library.GamePlayer.Hand
 {
     public class PlayerHand
     {
+        private int cardsLeft;
+
+        private bool isDone;
+
+        private List<Card> playerHand = new List<Card>();
     }
 }
