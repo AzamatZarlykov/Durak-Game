50cafe000cf66a22110d8c51808ed62d761adc31
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-07-31 23:59:45 +0600

    add game view class

---
+++ DurakGame.WebSite/DurakGame.Server/JSONHelper/GameView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DurakGame.Server.Library.GameCard;
+
+namespace DurakGame.Server.Helper
+{
+    public class GameView
+    {
+        // Information about the player
+        public int id;
+        public bool isAttacking;
+        public bool isDefending;
+        public List<Card> hand;
+
+        // Information about other players and the game
+        public int DeckSize;
+        public int DiscardHeapSize;
+        // Add how many cards does each player have 
+    }
+}
